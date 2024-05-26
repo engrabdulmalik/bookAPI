8e2893a99fd6eed7ebc3d52cc0a3c27f446f3d7c
engrabdulmalik <engrabdulmaliq@gmail.com>
2024-05-26 08:34:11 +0500

    new project

---
+++ booklistAPI/views.py
@@ -0,0 +1,31 @@
+from django.db import IntegrityError
+from django.http import HttpResponse, JsonResponse
+from .models import Book
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from django.forms.models import model_to_dict
+
+
+# Create your views here.
+@api_view(['GET', 'POST'])
+# @csrf_exempt
+
+def books(request):
+    if request.method == 'GET':
+        books = Book.objects.all().values()
+        return JsonResponse({"books":list(books)})
+    elif request.method == 'POST':
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        price = request.POST.get('price')
+        book = Book(
+            title = title,
+            author = author,
+            price = price
+        )
+        try:
+            book.save()
+        except IntegrityError:
+            return JsonResponse({'error':'true','message':'required field missing'},status=400)
+
+        return JsonResponse(model_to_dict(book), status=201)
